9fdcf5957eecb13641d738a7309b7a886c77c71e
Saicharan <saicharan14996@gmail.com>
2019-08-06 13:21:27 +0530

    Add files via upload

---
+++ assets/js/ref.js
@@ -0,0 +1,32 @@
+function namefunc() {
+  var namerep = document.getElementById("nameinput").value;
+  document.getElementById("name").innerHTML = namerep;
+}
+function mailfunc() {
+  var mailrep = document.getElementById("mailinput").value;
+  document.getElementById("mail").innerHTML = mailrep;
+}
+function numfunc() {
+  var numrep = document.getElementById("numinput").value;
+  document.getElementById("number").innerHTML = numrep;
+}
+function descfunc() {
+  var descrep = document.getElementById("descinput").value;
+  document.getElementById("desc").innerHTML = descrep;
+}
+function levelfunc() {
+  var levelrep = document.getElementById("levelinput").value;
+  document.getElementById("level").innerHTML = levelrep;
+}
+function descfunc() {
+  var descrep = document.getElementById("descinput").value;
+  document.getElementById("desc").innerHTML = descrep;
+}
+function ctcfunc() {
+  var ctcrep = document.getElementById("ctcinput").value;
+  document.getElementById("ctc").innerHTML = ctcrep;
+}
+function addfunc() {
+  var addrep = document.getElementById("addinput").value;
+  document.getElementById("add").innerHTML = addrep;
+}
